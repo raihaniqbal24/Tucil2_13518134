98f8d02a9fe8e862aec2cd47618ffd41fb225743
Muhammad Raihan Iqbal <raihanvega24@gmail.com>
2023-03-01 11:00:59 +0700

    add function to calculate points in grey area

--- src/main.py
+++ src/main.py
@@ -9,6 +9,10 @@ def sortListOfPoints(points):
   sortedPoints = sorted(points, key=itemgetter(0,1))
   return sortedPoints
 
+# menghitung jarak euclidean
+def euclidean_distance(p1, p2):
+  return math.sqrt(pow((p1[0] - p2[0]), 2) + pow((p1[1] - p2[1]), 2) + pow((p1[2] - p2[2]), 2))
+
 # brute force
 def closestPairBruteForce(thePoints):
   distance = float('inf')
@@ -17,7 +21,7 @@ def closestPairBruteForce(thePoints):
   for i in range(len(thePoints)):
     for j in range(len(thePoints)):
       if (thePoints[i] != thePoints[j]):
-        temp_distance = math.sqrt(pow((thePoints[i][0] - thePoints[j][0]), 2) + pow((thePoints[i][1] - thePoints[j][1]), 2) + pow((thePoints[i][2] - thePoints[j][2]), 2))
+        temp_distance = euclidean_distance(thePoints[i], thePoints[j])
         count += 1
         if (temp_distance < distance):
           distance = temp_distance
@@ -35,18 +39,50 @@ def closestPairDnC(thePoints):
     ptsCount = len(thePoints)
     pts1 = []
     pts2 = []
+    midPts = thePoints[int(ptsCount//2)]
+    min_distance = 0
     for i in range(int(ptsCount//2)):
       pts1.append(thePoints[i])
     for i in range(int(ptsCount//2), ptsCount):
       pts2.append(thePoints[i])
     d1, p11, p12, c1 = closestPairDnC(pts1)
     d2, p21, p22, c2 = closestPairDnC(pts2)
-    if (d1 > d2):
-      return d2, p21, p22, c2
+    if (d1 < d2):
+      min_distance = d1
     else:
-      return d1, p11, p12, c1
-    # greyArea = []
+      min_distance = d2
+    
+    greyArea = []
+    for i in range(len(thePoints)):
+      if ((thePoints[i] != midPts[0]) and (abs(thePoints[i][0] - midPts[0]) < min_distance) and (abs(thePoints[i][1] - midPts[1]) < min_distance)):
+        greyArea.append(thePoints[i])
+    
+    gDist, gp1, gp2, cg = closestPairGreyArea(greyArea, min_distance)
+    if (gDist < min_distance):
+      return gDist, gp1, gp2, (c1+c2+cg)
+    else:
+      if (min_distance == d1):
+        return d1, p11, p12, (c1+c2+cg)
+      else:
+        return d2, p21, p22, (c1+c2+cg)
 
+def closestPairGreyArea(thePoints, dist):
+  distance = dist
+  p = []
+  count = 0
+  grey = sorted(thePoints, key=itemgetter(2))
+  for i in range(len(grey)):
+    for j in range(len(grey)):
+      if (grey[i] != grey[j]):
+        temp_distance = euclidean_distance(grey[i], grey[j])
+        count += 1
+        if( temp_distance < distance):
+          distance = temp_distance
+          p.clear()
+          p.append(grey[i])
+          p.append(grey[j])
+
+  return distance, p[0], p[1], count
 
 
 if __name__ == "__main__":
